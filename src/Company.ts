355a18df975daa6b2972054ffccdafc1c53f2ca4
Diego Stiehl <seccomiro@gmail.com>
2020-06-05 13:44:39 -0300

    Make Company basic structure

--- src/Company.ts
+++ src/Company.ts
@@ -1,3 +1,19 @@
-class Company {
-  
+import faker from 'faker';
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.companyName();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
