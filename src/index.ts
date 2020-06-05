33b8d87d79149c296a4d58a4800f989cda69b561
Diego Stiehl <seccomiro@gmail.com>
2020-06-05 13:45:05 -0300

    Add Google Maps

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 import { User } from './User';
+import { Company } from './Company';
 
+const user = new User();
+console.log(user);
+
+const company = new Company();
+console.log(company);
+
+const map = new google.maps.Map(document.querySelector('.map'), {
+  zoom: 1,
+  center: user.location,
+});
