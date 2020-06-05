da43cbcb924f1d6c052459364092fa7c48814747
Diego Stiehl <seccomiro@gmail.com>
2020-06-05 14:49:01 -0300

    Create CustomMap and Mappable

---
+++ src/CustomMap.ts
@@ -0,0 +1,32 @@
+export interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+  color: string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(element: string) {
+    this.googleMap = new google.maps.Map(document.querySelector(element), {
+      zoom: 1,
+      center: { lat: 0, lng: 0 },
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
